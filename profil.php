cfa1bd54cf4e5a00282479c17f8f3b880e7612ca
Serge Razzini <eva.razzini@laplateforme.io>
2023-06-07 10:05:15 +0200

    connexion crée style fini

--- profil.php
+++ profil.php
@@ -1,13 +1,59 @@
 <?php
+// Démarrer la session
 session_start();
 
-if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
-    // Redirection vers la page de connexion si l'utilisateur n'est pas connecté
-    header("Location: connexion.php");
-    exit();
+// Récupérer les informations de l'utilisateur connecté depuis la base de données
+$host = "localhost";
+$dbname = "moduleconnexion";
+$username = "pma";
+$passwordDB = "plomkiplomki";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $passwordDB);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Récupérer les informations de l'utilisateur connecté
+    $query = "SELECT login, prenom, nom FROM utilisateurs WHERE login = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->execute([$_SESSION["login"]]);
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // Vérifier si le formulaire de mise à jour a été soumis
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Récupérer les nouvelles données du formulaire
+        $newLogin = $_POST["login"];
+        $newPrenom = $_POST["prenom"];
+        $newNom = $_POST["nom"];
+        $newPassword = $_POST["password"];
+
+        // Mettre à jour les informations de l'utilisateur dans la base de données
+        $updateQuery = "UPDATE utilisateurs SET login = ?, prenom = ?, nom = ?, password = ? WHERE login = ?";
+        $updateStmt = $conn->prepare($updateQuery);
+        $updateStmt->execute([$newLogin, $newPrenom, $newNom, $newPassword, $_SESSION["login"]]);
+
+        // Mettre à jour le login de l'utilisateur dans la variable de session
+        $_SESSION["login"] = $newLogin;
+
+        // Redirection vers la page de profil mise à jour
+        header("Location: profil.php");
+        exit;
+    }
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
 }
 
-// Code pour mettre à jour les informations du profil dans la base de données
+// Traitement de la déconnexion
+if (isset($_GET["logout"])) {
+    // Supprimer toutes les variables de session
+    session_unset();
+
+    // Détruire la session
+    session_destroy();
+
+    // Redirection vers la page de connexion
+    header("Location: connexion.php");
+    exit;
+}
 ?>
 
 <!DOCTYPE html>
@@ -19,22 +65,27 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
     @import url('https://fonts.googleapis.com/css2?family=Bruno+Ace+SC&display=swap');
   </style>
 </head>
-<body class="bodyprof" >
-    <form method="POST" action="profil.php">
+<body class="bodyprof">
+    <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
         <h1>Profil</h1>
-        <label style="font-size: 0.5em" for="login">Login :</label>
-        <input type="text" id="login" name="login" value="<?php echo $_SESSION["login"]; ?>" required><br>
+        <label for="login" style="font-size: 0.5em">Login:</label>
+        <input type="text" id="login" name="login" value="<?php echo $row["login"]; ?>" required><br>
 
-        <label style="font-size: 0.5em" for="prenom">Prénom :</label>
-        <input type="text" id="prenom" name="prenom" value="<?php echo $_SESSION["prenom"]; ?>" required><br>
+        <label for="prenom" style="font-size: 0.5em">Prénom:</label>
+        <input type="text" id="prenom" name="prenom" value="<?php echo $row["prenom"]; ?>" required><br>
 
-        <label style="font-size: 0.5em" for="nom">Nom  :</label>
-        <input type="text" id="nom" name="nom" value="<?php echo $_SESSION["nom"]; ?>" required><br>
+        <label for="nom" style="font-size: 0.5em">Nom:</label>
+        <input type="text" id="nom" name="nom" value="<?php echo $row["nom"]; ?>" required><br>
 
-        <label style="font-size: 0.5em" for="password">Mot de passe :</label>
-        <input type="text" id="password" name="password" value="<?php echo $_SESSION["password"]; ?>" required><br>
+        <label for="password" style="font-size: 0.5em">Nouveau mot de passe:</label>
+        <input type="password" id="password" name="password" required><br>
 
-        <input type="submit" value="Enregistrer" >
+        <input type="submit" value="Enregistrer les modifications">
+    </form>
+    <br>
+    <form method="GET" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+        <input type="hidden" name="logout" value="true">
+        <input type="submit" value="Se déconnecter">
     </form>
 </body>
 </html>
